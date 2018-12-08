43c200478dca75a29f4556eb5b9c24ad7cf48ff6
David Scales <davidsscales@gmail.com>
2018-12-08 15:44:32 -0800

    feat: ExistingListItemForm which invalidates duplicate items

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,8 @@ from lists.views import home_page
 from lists.models import Item, List
 from lists.forms import ItemForm
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
+from unittest import skip
 
 class HomePageTest(TestCase):
 
@@ -96,6 +97,19 @@ class ListViewTest(TestCase):
     response = self.client.get(f'/lists/{list_.id}/') # hardcoded?
     self.assertIsInstance(response.context['form'], ItemForm)
     self.assertContains(response, 'name="text"')
+
+  @skip
+  def test_ducplicate_item_validation_errors_end_up_on_lists_page(self):
+    list1 = List.objects.create()
+    item1 = Item.objects.create(list = list1, text = 'textey')
+    response = self.client.post(
+      f'/lists/{list1.id}/', # hardcoded?
+      data = { 'text': 'textey' }
+    )
+    expected_error = escape(DUPLICATE_ITEM_ERROR)
+    self.assertContains(response, exected_error)
+    self.assertTemplateUsed(response, 'list.html')
+    self.assertEqual(Item.objects.all.count(), 1)
 
 class NewListTest(TestCase):
 
