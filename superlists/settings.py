95ebd8a80332dd9884b7c4767cc903d04f1f1193
David Scales <davidsscales@gmail.com>
2019-01-13 16:53:10 -0800

    feat: custom passwordless auth backend + custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -46,6 +46,15 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+]
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'davidsprojectstesting@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
