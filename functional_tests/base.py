4f059c70209af9cedfd385bbf82e49a95f184cdc
David Scales <davidsscales@gmail.com>
2019-02-18 14:16:42 -0800

    FT for sharing, move session creation helper to base FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+from .server_tools import create_session_on_server
+from .management.commands.create_session import create_pre_authenticated_session
+from django.conf import settings
 
 import os
 from datetime import datetime
@@ -105,3 +108,17 @@ class FunctionalTest(StaticLiveServerTestCase):
     self.browser.find_element_by_name('email')
     navbar = self.browser.find_element_by_css_selector('.navbar')
     self.assertNotIn(email, navbar.text)
+
+  def create_pre_authenticated_session(self, email):
+    if self.staging_server:
+      session_key = create_session_on_server(self.staging_server, email)
+    else:
+      session_key = create_pre_authenticated_session(email)
+    ## to set a cookie we need to first visit the domain.
+    ## 404 pages load the quickest!
+    self.browser.get(self.live_server_url + "/404_no_such_url/")
+    self.browser.add_cookie(dict(
+      name=settings.SESSION_COOKIE_NAME,
+      value=session_key,
+      path='/',
+    ))
