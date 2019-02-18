03bc2b2e26ac4cd399ffc9934339418059a850a9
David Scales <davidsscales@gmail.com>
2019-02-18 12:02:41 -0800

    Bump timeout for FT wait helper

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -13,7 +13,7 @@ from datetime import datetime
 SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
 )
-MAX_WAIT = 15
+MAX_WAIT = 20
 
 class FunctionalTest(StaticLiveServerTestCase):
 
